4a6c1959b4e143e019a8b7aca3dee13fbcb33e2d
Dustin Morrill <dmorrill10@gmail.com>
2013-04-23 22:19:32 -0600

    Started adding new gamedef. Not working yet.

--- lib/acpc_dealer.rb
+++ lib/acpc_dealer.rb
@@ -2,13 +2,16 @@ require File.expand_path('../hand_evaluator', __FILE__)
 require File.expand_path("../acpc_dealer/dealer_runner", __FILE__)
 
 module AcpcDealer
-  DEALER_DIRECTORY = File.expand_path('../../vendor/project_acpc_server', __FILE__)
+  VENDOR_DIRECTORY = File.expand_path('../../vendor', __FILE__)
+  DEALER_DIRECTORY = File.join(VENDOR_DIRECTORY, 'project_acpc_server')
+  GAME_DEF_DIRECTORY = File.join(VENDOR_DIRECTORY, 'game_defs')
   GAME_DEFINITION_FILE_PATHS =
   {
     2 =>
     {
       limit: "#{DEALER_DIRECTORY}/holdem.limit.2p.reverse_blinds.game",
-      nolimit: "#{DEALER_DIRECTORY}/holdem.nolimit.2p.reverse_blinds.game"
+      nolimit: "#{DEALER_DIRECTORY}/holdem.nolimit.2p.reverse_blinds.game",
+      nolimit100b: File.join(GAME_DEF_DIRECTORY, 'game_def.holdem.2p.no_limit.100BB.reverse_blinds')
     },
     3 =>
     {
